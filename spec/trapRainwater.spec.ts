8d847bb4985447780a074110c51f9b7aa6064c7e
asarudick <asa.rudick@gmail.com>
2020-08-24 10:40:55 -0700

    update trap rainwater exercise to include dp solution

--- spec/trapRainwater.spec.ts
+++ spec/trapRainwater.spec.ts
@@ -1,7 +1,7 @@
-import trapRainwater from '../src/trapRainwater';
+import { trapRainwater, trapRainwaterDp } from '../src/trapRainwater';
 import { it, expect, describe } from '@jest/globals';
 
-describe('', () => {
+describe('trapRainwater', () => {
   it('should return []', () => {
     const expected = [];
     const result = trapRainwater([]);
@@ -50,6 +50,59 @@ describe('', () => {
   it('should return [0, 2, 0, 1, 1, 0]', () => {
     const expected = [0, 2, 0, 1, 1, 0];
     const result = trapRainwater([3, 0, 2, 0, 0, 1]);
+    expect(result).toEqual(result);
+  });
+});
+
+describe('trapRainwaterDp', () => {
+  it('should return []', () => {
+    const expected = [];
+    const result = trapRainwaterDp([]);
+    expect(result).toEqual(result);
+  });
+  it('should return [0]', () => {
+    const expected = [0];
+    const result = trapRainwaterDp([0]);
+    expect(result).toEqual(result);
+  });
+  it('should return [0, 1]', () => {
+    const expected = [0, 1];
+    const result = trapRainwaterDp([1, 0]);
+    expect(result).toEqual(result);
+  });
+  it('should return [1, 0]', () => {
+    const expected = [1, 0];
+    const result = trapRainwaterDp([0, 1]);
+    expect(result).toEqual(result);
+  });
+  it('should return [0, 1, 0]', () => {
+    const expected = [0, 1, 0];
+    const result = trapRainwaterDp([1, 0, 1]);
+    expect(result).toEqual(result);
+  });
+  it('should return [0, 1, 0]', () => {
+    const expected = [0, 1, 0];
+    const result = trapRainwaterDp([2, 0, 1]);
+    expect(result).toEqual(result);
+  });
+  it('should return [0, 1, 0]', () => {
+    const expected = [0, 1, 0];
+    const result = trapRainwaterDp([1, 0, 2]);
+    expect(result).toEqual(result);
+  });
+  it('should return [1, 0, 1, 0]', () => {
+    const expected = [1, 0, 1, 0];
+    const result = trapRainwaterDp([0, 1, 0, 2]);
+    expect(result).toEqual(result);
+  });
+  it('should return [0, 1, 0, 1]', () => {
+    const expected = [0, 1, 0, 1];
+    const result = trapRainwaterDp([1, 0, 2, 0]);
+    expect(result).toEqual(result);
+  });
+  it('should return [0, 2, 0, 1, 1, 0]', () => {
+    const expected = [0, 2, 0, 1, 1, 0];
+    const result = trapRainwaterDp([3, 0, 2, 0, 0, 1]);
     expect(result).toEqual(result);
   });
 });
--- src/trapRainwater.ts
+++ src/trapRainwater.ts
@@ -1,18 +1,43 @@
-export default function (heights: number[]) {
-    const water = [];
+export function trapRainwater(heights: number[]) {
+  const water: number[] = [];
 
-    for (let i = 0; i < heights.length; i++) {
-        let maxLeft = heights[i], maxRight = heights[i];
-        for (let j = i; j < heights.length; j++) {
-            maxRight = Math.max(heights[j], maxRight);
-        }
-        for (let j = i; j >= 0; j--) {
-            maxLeft = Math.max(heights[j], maxLeft);
-        }
-
-        const min = Math.min(maxLeft, maxRight);
-        water.push(min - heights[i]);
+  for (let i = 0; i < heights.length; i++) {
+    let maxLeft = heights[i],
+      maxRight = heights[i];
+    for (let j = i; j < heights.length; j++) {
+      maxRight = Math.max(heights[j], maxRight);
+    }
+    for (let j = i; j >= 0; j--) {
+      maxLeft = Math.max(heights[j], maxLeft);
     }
 
-    return water;
+    const min = Math.min(maxLeft, maxRight);
+    water.push(min - heights[i]);
+  }
+
+  return water;
+}
+
+export function trapRainwaterDp(heights: number[]) {
+  const water: number[] = [];
+  const left: number[] = [];
+  const right: number[] = [];
+
+  let maxSoFar = 0;
+
+  for (let i = 0; i < heights.length; i++) {
+    maxSoFar = Math.max(maxSoFar, heights[i]);
+    left.push(maxSoFar);
+  }
+
+  for (let i = heights.length - 1; i >= 0; i--) {
+    maxSoFar = Math.max(maxSoFar, heights[i]);
+    right.push(maxSoFar);
+  }
+
+  for (let i = 0; i < heights.length; i++) {
+    water.push(Math.max(left[i], right[i]) - heights[i]);
+  }
+
+  return water;
 }
