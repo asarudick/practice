e05afd0d73f5d7baed0d286e7bac3595eb73513d
asarudick <asa.rudick@gmail.com>
2020-09-02 20:22:15 -0700

    add flatten exercise using stack

--- spec/flatten.spec.ts
+++ spec/flatten.spec.ts
@@ -1,4 +1,4 @@
-import flatten from '../src/flatten';
+import { flatten, flattenStack } from '../src/flatten';
 import { it, expect, describe } from '@jest/globals';
 
 describe('flatten', () => {
@@ -35,6 +35,43 @@ describe('flatten', () => {
   it('should return [0, 0] for [[[0]], [0]]', () => {
     const expected: any = [0, 0];
     const result = flatten([[[0]], [0]]);
+    expect(result).toEqual(expected);
+  });
+});
+describe('flattenStack', () => {
+  it('should return [] for []', () => {
+    const expected: any = [];
+    const result = flattenStack([]);
+    expect(result).toEqual(expected);
+  });
+  it('should return [0] for [0]', () => {
+    const expected: any = [0];
+    const result = flattenStack([0]);
+    expect(result).toEqual(expected);
+  });
+  it('should return [0] for [[0]]', () => {
+    const expected: any = [0];
+    const result = flattenStack([[0]]);
+    expect(result).toEqual(expected);
+  });
+  it('should return [0] for [[[0]]]', () => {
+    const expected: any = [0];
+    const result = flattenStack([[[0]]]);
+    expect(result).toEqual(expected);
+  });
+  it('should return [0, 0] for [[0], 0]', () => {
+    const expected: any = [0, 0];
+    const result = flattenStack([[0], 0]);
+    expect(result).toEqual(expected);
+  });
+  it('should return [0, 0] for [[0], [0]]', () => {
+    const expected: any = [0, 0];
+    const result = flattenStack([[0], [0]]);
+    expect(result).toEqual(expected);
+  });
+  it('should return [0, 0] for [[[0]], [0]]', () => {
+    const expected: any = [0, 0];
+    const result = flattenStack([[[0]], [0]]);
     expect(result).toEqual(expected);
   });
 });
--- src/flatten.ts
+++ src/flatten.ts
@@ -1,7 +1,23 @@
-export default function flatten(value: any): any {
+export function flatten(value: any): any {
   if (Array.isArray(value)) {
     return value.reduce((a, b) => a.concat(flatten(b)), []);
   }
 
   return value;
+}
+
+export function flattenStack(arr: any[]): any {
+  const stack: any[] = [...arr];
+  let result: any[] = [];
+
+  while (stack.length) {
+    const item = stack.pop();
+    if (Array.isArray(item)) {
+      stack.push(...item);
+    } else {
+      result.push(item);
+    }
+  }
+
+  return result;
 }
